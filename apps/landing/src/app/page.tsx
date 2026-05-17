2385195055c25941bd0ffe0852cc48e4a9467f13
Riccardo <me@riccardo.io>
2026-05-17 02:02:43 +0100

    feat(landing): collect beta access requests

--- apps/landing/src/app/page.tsx
+++ apps/landing/src/app/page.tsx
@@ -1,15 +1,14 @@
-import { SiteNav } from "@/components/SiteNav";
-import { HeroSection } from "@/components/HeroSection";
-import { SectionDivider } from "@/components/SectionDivider";
-import { HowItWorks } from "@/components/HowItWorks";
-import { FeaturesSection } from "@/components/FeaturesSection";
-import { IntegrationSection } from "@/components/IntegrationSection";
-import { ArchitectureSection } from "@/components/ArchitectureSection";
-import { PricingSection } from "@/components/PricingSection";
-import { FaqSection } from "@/components/FaqSection";
-import { CtaSection } from "@/components/CtaSection";
-import { SiteFooter } from "@/components/SiteFooter";
-import { Reveal } from "@/components/Reveal";
+import { SiteNav } from '@/components/SiteNav';
+import { HeroSection } from '@/components/HeroSection';
+import { SectionDivider } from '@/components/SectionDivider';
+import { HowItWorks } from '@/components/HowItWorks';
+import { FeaturesSection } from '@/components/FeaturesSection';
+import { IntegrationSection } from '@/components/IntegrationSection';
+import { ArchitectureSection } from '@/components/ArchitectureSection';
+import { FaqSection } from '@/components/FaqSection';
+import { CtaSection } from '@/components/CtaSection';
+import { SiteFooter } from '@/components/SiteFooter';
+import { Reveal } from '@/components/Reveal';
 
 export default function Home() {
   return (
@@ -31,10 +30,6 @@ export default function Home() {
       <SectionDivider />
       <Reveal>
         <ArchitectureSection />
-      </Reveal>
-      <SectionDivider />
-      <Reveal>
-        <PricingSection />
       </Reveal>
       <SectionDivider />
       <Reveal>
